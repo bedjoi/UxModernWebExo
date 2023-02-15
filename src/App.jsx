eeb60a4203ca72088c55582fd345f0bb9f4e54b8
bedjo <obedindevu@gmail.com>
2023-02-15 16:09:32 +0200

    feat adding components

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,26 @@
-import React from 'react'
+import styles from "./style"
 
 export default function App() {
   return (
     <div className="bg-primary w-full overflow-hidden">
-      <h1 >Hello everyone</h1>
+      <div className={`${styles.paddingX} ${styles.flexCenter}`}>
+        <div className={`${styles.boxWidth}`}>
+            Navbar
+        </div>
+      </div>
+
+      <div className={`bg-primary ${styles.paddingX} ${styles.flexStart}`}>
+          <div className={`${styles.boxWidth}`}>
+            Stats
+            Busness
+            Billing
+            CardDeal
+            Testimonials
+            Client
+            CTA
+            Footer 
+          </div>
+      </div>
     </div>
   )
 };
