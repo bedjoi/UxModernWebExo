4edde7aad0d7942f4e4dab5c1b9d209c24293dc9
bedjo <obedindevu@gmail.com>
2023-02-15 16:27:16 +0200

    fix: name of components

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,30 @@
-import styles from "./style"
+import {Billing, Business, CardDeal, Clients,CTA, Footer, Hero, Navbar, Stats, Testimonials} from "./components";
+import styles from "./style";
 
 export default function App() {
   return (
     <div className="bg-primary w-full overflow-hidden">
       <div className={`${styles.paddingX} ${styles.flexCenter}`}>
         <div className={`${styles.boxWidth}`}>
-            Navbar
+            <Navbar/>
+        </div>
+      </div>
+      <div className={`${styles.paddingX} ${styles.flexCenter}`}>
+        <div className={`${styles.boxWidth}`}>
+            <Hero/>
         </div>
       </div>
 
       <div className={`bg-primary ${styles.paddingX} ${styles.flexStart}`}>
           <div className={`${styles.boxWidth}`}>
-            Stats
-            Busness
-            Billing
-            CardDeal
-            Testimonials
-            Client
-            CTA
-            Footer 
+            <Stats/>
+            <Business/>
+            <Billing/>
+            <CardDeal/>
+            <Testimonials/>
+            <Clients/>
+            <CTA/>
+            <Footer/>
           </div>
       </div>
     </div>
